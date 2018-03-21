610a9637f44e21339b15326c1c70f724d510624a
brukk <bruk1892@gmail.com>
2018-03-21 12:57:45 -0600

    object practice added

--- business/js.js
+++ business/js.js
@@ -1,10 +1,40 @@
 var shopper={
-     shoe:"nike",
-     height:184,
-     groceryCart:["banana", "orange", "apple","onion"],
-     theShopper:function (){
-                return "the shopper wears " + this.shoe+ " shoe and he is "+this.height+"cms tall";
-     }
+    shoe:"nike",
+    height:184,
+    groceryCart:["banana", "orange", "apple","onion"],
+    theShopper:function (){
+               return "the shopper wears " + this.shoe+ " shoe and he is "+this.height+"cms tall";
+    }
+}
+console.log(shopper.theShopper())
+
+//************************************************** */
+
+var bed={
+   confi:true,
+   width:2,
+   color:"red",
+   accessaries:["pillow","sheets","matrice"],
+   myBed:function(){
+       return "is the bed confortable? the answer is " + this.confi+ " and its color is "+this.color+ 
+              " and it has " +this.accessaries;
+   }
 }
 
-console.log(shopper.theShopper())
+console.log(bed.myBed())
+
+// ************************************************************************
+var tv ={
+    weight:5,
+    width:32,
+    highDef:false,
+    color:"black",
+    thisTv:function(){
+        return "this TV is " +this.width+"inch with "+ this.color+" color with "
+                + this.features;
+    },
+    features:["4k","HDMI port", "super slim"]
+}
+
+console.log(tv.thisTv())
+
