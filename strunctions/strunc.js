c19da043421204813161c01cd17d75ef7a0df28e
brukk <bruk1892@gmail.com>
2018-03-23 14:01:03 -0600

    js string method prac added

--- strunctions/strunc.js
+++ strunctions/strunc.js
@@ -1,14 +1,14 @@
 function firstF(x,y){
     return x.concat(y);
 }
-console.log(firstF("i am", " hot"))
+console.log(firstF("i am", " Habesha"))
 
 var obj1={
     name:"Bk",
     age:30,
     coo:"Eth",
     disc:function(){
-        return this.name.concat(" is a Gem ").concat("and he's ".concat(this.age).concat(" years old"))
+        return this.name.concat(" is a Habesh ").concat("and he's ".concat(this.age).concat(" years old"))
     }
 }
 console.log(obj1.disc())
@@ -33,3 +33,22 @@ function forth(str){
     return str.substr(2,6)
 }
 console.log(forth("greeen is the color"))
+
+
+
+//no duplicates
+var input="bookkeeper larry";
+function myFunc(str){
+    var output={
+                 nonD:"",
+                 dup:""
+                };
+    for(var i=0; i<str.length; i++){
+        if(str.indexOf(str[i])===i){
+            output.nonD=output.nonD+str[i];
+        }else if(str.indexOf(str[i])!==i){
+            output.dup=output.dup+str[i]
+        }
+    }return output
+}
+console.log(myFunc("bookkeeper larry"))
