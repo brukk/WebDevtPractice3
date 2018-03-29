a827e1803fa29c5b092237ccd75ebba6256cc7e3
brukk <bruk1892@gmail.com>
2018-03-29 14:29:27 -0600

    random password function added

--- randomPassword/password.js
+++ randomPassword/password.js
@@ -1,17 +1,32 @@
 // a function that generates a random password  with a parameter 
-    //    defining how long the password should be.
+   //    defining how long the password should be.
+function password(pwLength){
+    var characterSet="123456ABCDEFabcdef!@#$%&";
+    var password="";
+    for(var i=0; i<pwLength; i++){
+        var index=Math.floor(Math.random() * characterSet.length);
+        password=password+ characterSet.charAt(index);
+    }return password
+}
+console.log(password(8));
 
-    function password(pwLength){
-        var characterSet="123456ABCDEFabcdef!@#$%&";
-        var password="";
-        for(var i=0; i<pwLength; i++){
-            var index=Math.floor(Math.random() * characterSet.length);
-            password=password+ characterSet.charAt(index);
-        }return password
-    }
-    console.log(password(8));
-    
-    
+
+
+//a function generates a random password
+function randomPw(){
+    var password="";
+    var charSet="ABCDEF123456abcdef!@#$%&";
+    var passwordLen=8;
+
+    for(var i=0; i<passwordLen;i++){
+        password=password+ charSet.charAt(Math.floor( Math.random()*charSet.length));
+    }return password
+}
+
+console.log(randomPw());
+
+
+
     
     
     
