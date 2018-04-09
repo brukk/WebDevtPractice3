40b13c9cc19cce2c14b73b2100fa621e65b86bc2
brukk <bruk1892@gmail.com>
2018-04-08 19:49:18 -0600

    jquery on ninja file added

---
+++ ninja/ninja.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("h2").on("mouseenter" ,function(){
+       $("#middle article").slideToggle(2000)
+       .slideToggle(1200).slideToggle(1200)
+       .slideToggle(1200).slideToggle(600)
+       .slideDown(1200);
+        
+    
+    });
+
+
+
+})
