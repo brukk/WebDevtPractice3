f535c481e8b99a086085cc3abf793889912db238
brukk <bruk1892@gmail.com>
2018-04-09 13:40:42 -0600

    jquery on ninja dir edited

--- ninja/ninja.js
+++ ninja/ninja.js
@@ -5,19 +5,24 @@ $(document).ready(function(){
         
     // });
 
-    $("h2").on("click", function(){
-        $("#bottom article p").animate({
-            opacity:"0.4",height:"+=40px"}
-        ,1200);
-        $("#bottom article p").animate({
-            opacity:"0.8",fontSize:"18px"}
-        ,"fast")
-    });
+    // $("h2").on("click", function(){
+    //     $("#bottom article p").animate({
+    //         opacity:"0.4",height:"+=40px"}
+    //     ,1200);
+    //     $("#bottom article p").animate({
+    //         opacity:"0.8",fontSize:"18px"}
+    //     ,"fast")
+    // });
 
-    $("h2").on("mouseenter", function(){
-        console.log($("#menu .a").attr("href"));
-    })
+    // $("h2").on("mouseenter", function(){
+    //     console.log($("#menu .a").attr("href"));
+    // })
 
+    // $("h2").on("mouseenter", function(){
+    //     $("#banner .one").text(function(i, orig){
+    //          return orig + " PANDA"
+    //     })
+    // })
 
 
 })
