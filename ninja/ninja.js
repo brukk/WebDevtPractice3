3e847faac349aea1081f9c0028ff3ba89e64cf38
brukk <bruk1892@gmail.com>
2018-04-08 22:31:55 -0600

    jquery to ninja dir added

--- ninja/ninja.js
+++ ninja/ninja.js
@@ -7,8 +7,11 @@ $(document).ready(function(){
 
     $("h2").on("click", function(){
         $("#bottom article p").animate({
-            opacity:"0.4",height:"toggle"}
-        ,2000)
+            opacity:"0.4",height:"+=40px"}
+        ,1200);
+        $("#bottom article p").animate({
+            opacity:"0.8",fontSize:"18px"}
+        ,"fast")
     });
 
 
