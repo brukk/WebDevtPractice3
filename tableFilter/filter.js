b73d7877aa36474c0d138070dc878357a7faddc3
brukk <bruk1892@gmail.com>
2018-04-09 22:19:58 -0600

    table filter using jquery added

---
+++ tableFilter/filter.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#myInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+
+      $("#myTable tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        // The return value of the indexOf() function is the numeric 
+          // index of that value in the target value, or -1 if it's  
+            // not found. So for each value that we're looking for, 
+              // we'd want to check if it's index is > -1 
+                // (which means it's found in the string(value)). 
+        
+      });
+    });
+
+
+});
