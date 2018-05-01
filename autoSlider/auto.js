b4101a360b2f04ec0c11eab37fc14c84017877e3
brukk <bruk1892@gmail.com>
2018-04-30 22:27:46 -0600

    autoSlider using html css $ jquery added

---
+++ autoSlider/auto.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    $(".slides img:first").addClass("top");
+
+    var change=function(){
+        var curr=$(".slides img.top");
+        var next=curr.next();
+
+        if(!next.length){
+            next=$(".slides img:first");
+            next.addClass("top");
+            curr.removeClass("top");
+        }else{
+            next.addClass("top");
+            curr.removeClass("top");
+        }
+    }
+
+    setInterval(change,1400);
+
+
+});
